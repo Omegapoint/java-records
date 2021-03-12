e143a26efb06ab3df8b875d670dd6308547be2a6
Haubir <haumar@omegapoint.se>
2021-03-12 14:05:20 +0100

    Nästa uppgift

---
+++ src/main/java/com/omegapoint/kompetenspass/pythagoras/PythagorasService.java
@@ -0,0 +1,13 @@
+package com.omegapoint.kompetenspass.pythagoras;
+
+import com.omegapoint.kompetenspass.pythagoras.pojos.DiagonalCalculator;
+import com.omegapoint.kompetenspass.pythagoras.pojos.DiagonalStats;
+import com.omegapoint.kompetenspass.pythagoras.records.DiagonalCalculatorRecord;
+import com.omegapoint.kompetenspass.pythagoras.records.DiagonalStatsRecord;
+
+public class PythagorasService {
+
+    public void run() {
+        // TODO: Skapa en diagonalCalculator som tar in en rektangels sidor och räknar ut diagonalen med hjälp av Pythagoras sats. Används Records för detta!
+    }
+}
